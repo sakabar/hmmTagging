0b75c9c5f897100b511028c9c6a2dbca21902235
sakabar <takafumi.sakakibara@gmail.com>
2014-04-10 18:34:22 +0900

    implemented get_num_of_pos_array

--- bin/utils.py
+++ bin/utils.py
@@ -10,4 +10,16 @@ def get_num_of_word_array(lst):
 
   return ans
 
+def get_num_of_pos_array(lst):
+  ans = {}
+
+  for item in lst:
+    (w, p) = item.split("/")
+    if p in ans:
+      ans[p] += 1
+    else:
+      ans[p] = 1
+
+  return ans
+
    

9f9f88914ccc56db694a22d5e4c4ae26a44924c5
sakabar <takafumi.sakakibara@gmail.com>
2014-04-11 10:22:20 +0900

    add Tagger class

---
+++ bin/main.py
@@ -0,0 +1,13 @@
+import sys
+import math
+import utils
+from utils import Tagger
+
+def main():
+  words = ["I", "have", "a", "book"]
+  wpList = map((lambda str: str.rstrip("\n")), sys.stdin.readlines())
+  tagger = Tagger(wpList)
+
+
+if __name__ == '__main__':
+  main()

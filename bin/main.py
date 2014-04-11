e9d9f98b7d19e3de2561532032c42a5668e2bd8f
sakabar <takafumi.sakakibara@gmail.com>
2014-04-12 00:38:08 +0900

    implemented processing of unknown words

--- bin/main.py
+++ bin/main.py
@@ -15,29 +15,20 @@ def main():
   wordsList = filter(lambda str: str != "", open("./data/entest").read().split("###/###"))
   testData = map(lambda str: filter(lambda str2: str2 != "", str.split("\n")), wordsList)
 
-  for k in tagger.n_pp.keys():
-    if k[0]=="###":
-      print tagger.n_pp[k]
 
-  sys.exit(0)
-  words = ["Sato", "likes" , "baseball", "."]
-  tagged = tagger.viterbi(words)
-  print tagged
-  sys.exit(0)
+  # words = ["I", "was", "born" , "on" , "Oct." , "13", "."]
+  # print tagger.viterbi(words)
+  # sys.exit(0)
 
-  
-  #checking
-  s, p = utils.get_pair_of_sentence_and_pos(testData[0])
-  print tagger.n_wp[("everyone", ",")]
-  sys.exit(0)
-  
-  for t in s:
-    print t,
-  tagged = tagger.viterbi(s)
+  # #checking
+  # s, p = utils.get_pair_of_sentence_and_pos(testData[1])
+  # for t in s:
+  #   print t,
+  # tagged = tagger.viterbi(s)
 
-  print zip(p, tagged)
-  sys.stdout.flush()
-  sys.exit(0)
+  # print zip(s, tagged)
+  # sys.stdout.flush()
+  # sys.exit(0)
 
   ok = 0
   ng = 0
